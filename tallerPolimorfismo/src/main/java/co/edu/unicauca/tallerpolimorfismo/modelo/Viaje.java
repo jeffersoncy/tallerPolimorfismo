38b81142498613ee2c9449b304885f5229ca242f
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2020-09-08 12:44:15 -0500

    terminando

--- tallerPolimorfismo/src/main/java/co/edu/unicauca/tallerpolimorfismo/modelo/Viaje.java
+++ tallerPolimorfismo/src/main/java/co/edu/unicauca/tallerpolimorfismo/modelo/Viaje.java
@@ -5,10 +5,89 @@
  */
 package co.edu.unicauca.tallerpolimorfismo.modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Personal
  */
-public class Viaje {
+public abstract class Viaje {
     
+    private String atrOrigen;
+    private String atrDestino;
+    private int atrCosto;
+    private Date atrFechaSalida;
+    private Date atrFechaLlegada;
+
+    
+    /**
+     * @return the atrOrigen
+     */
+    public String getAtrOrigen() {
+        return atrOrigen;
+    }
+    
+
+    /**
+     * @param atrOrigen the atrOrigen to set
+     */
+    public void setAtrOrigen(String atrOrigen) {
+        this.atrOrigen = atrOrigen;
+    }
+
+    /**
+     * @return the atrDestino
+     */
+    public String getAtrDestino() {
+        return atrDestino;
+    }
+
+    /**
+     * @param atrDestino the atrDestino to set
+     */
+    public void setAtrDestino(String atrDestino) {
+        this.atrDestino = atrDestino;
+    }
+
+    /**
+     * @return the atrCosto
+     */
+    public int getAtrCosto() {
+        return atrCosto;
+    }
+
+    /**
+     * @param atrCosto the atrCosto to set
+     */
+    public void setAtrCosto(int atrCosto) {
+        this.atrCosto = atrCosto;
+    }
+
+    /**
+     * @return the atrFechaSalida
+     */
+    public Date getAtrFechaSalida() {
+        return atrFechaSalida;
+    }
+
+    /**
+     * @param atrFechaSalida the atrFechaSalida to set
+     */
+    public void setAtrFechaSalida(Date atrFechaSalida) {
+        this.atrFechaSalida = atrFechaSalida;
+    }
+
+    /**
+     * @return the atrFechaLlegada
+     */
+    public Date getAtrFechaLlegada() {
+        return atrFechaLlegada;
+    }
+
+    /**
+     * @param atrFechaLlegada the atrFechaLlegada to set
+     */
+    public void setAtrFechaLlegada(Date atrFechaLlegada) {
+        this.atrFechaLlegada = atrFechaLlegada;
+    }
 }
